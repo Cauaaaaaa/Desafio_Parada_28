b0c366b52d9b6cb866d2eaf4d088cbdb4b59c33d
CauãZão <163067312+Cauaaaaaa@users.noreply.github.com>
2024-06-11 23:17:52 -0300

    Add files via upload

---
+++ Desafio_Parada_28/js/script.js
@@ -0,0 +1,27 @@
+var imagens_index = 1;
+mostrar_imagens(imagens_index);
+function current_slides(num){
+    mostrar_imagens(imagens_index = num)
+}
+function passar_imagens(num){
+    mostrar_imagens(imagens_index += num)
+}
+function mostrar_imagens(num){
+
+    var imagens = document.getElementsByClassName("fade");
+    var i;
+
+    if(num<1){
+        imagens_index = imagens.length;
+    }
+    
+    if(num>imagens.length){
+        imagens_index = 1;
+    }
+
+    for(i=0; i<imagens.length; i++){
+        imagens[i].style.display = "none"
+    }
+
+    imagens[imagens_index-1].style.display = "block";
+}
